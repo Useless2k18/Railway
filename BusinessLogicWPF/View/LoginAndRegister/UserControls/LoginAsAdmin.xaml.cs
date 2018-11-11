461943925e539c179938ab5c4bfc13e5eb026b4d
Sourodeep Chatterjee <sourodeep224@outlook.com>
2018-11-11 14:48:05 +0530

    Added LoginAsAdmin View

--- BusinessLogicWPF/View/LoginAndRegister/UserControls/LoginAsAdmin.xaml.cs
+++ BusinessLogicWPF/View/LoginAndRegister/UserControls/LoginAsAdmin.xaml.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using BusinessLogicWPF.ViewModel.LoginAndRegister;
 using System.Linq;
-using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace BusinessLogicWPF.View.LoginAndRegister.UserControls
 {
@@ -20,9 +14,91 @@ namespace BusinessLogicWPF.View.LoginAndRegister.UserControls
     /// </summary>
     public partial class LoginAsAdmin : UserControl
     {
+        private readonly System.Windows.Window _window;
+
         public LoginAsAdmin()
         {
             InitializeComponent();
+            _window = Application.Current.MainWindow;
         }
+
+        private void ButtonBack_OnClick(object sender, RoutedEventArgs e)
+        {
+            _window.DataContext = new LoginViewModel();
+        }
+
+        private void ButtonLogin_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Logged in!");
+        }
+
+        #region Input Fields
+
+        private async void TextUserName_LostKeyboardFocus(object sender, System.Windows.Input.KeyboardFocusChangedEventArgs e)
+        {
+            if (TextUserName.Text.Length == 0) return;
+
+            await Task.Factory.StartNew(() =>
+            {
+                string text = null;
+
+                Application.Current.Dispatcher.Invoke(DispatcherPriority.Normal,
+                    (ThreadStart)delegate { text = TextUserName.Text; });
+
+
+                if (text == "sdc224")
+                    MessageBox.Show($"Hi {text}");
+            });
+        }
+
+        private void TextPassword_LostKeyboardFocus(object sender, System.Windows.Input.KeyboardFocusChangedEventArgs e)
+        {
+            if (TextPassword.Password.Length != 0)
+                MessageBox.Show($"You just entered {TextPassword.Password}");
+        }
+
+        private static readonly Regex Regex = new Regex("[^0-9]+"); //regex that matches disallowed text
+        private static bool IsTextAllowed(string text)
+        {
+            return !Regex.IsMatch(text);
+        }
+
+        private void TextOtp_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextAllowed(e.Text);
+        }
+
+        private void TextOtp_OnPasting(object sender, DataObjectPastingEventArgs e)
+        {
+            if (e.DataObject.GetDataPresent(typeof(string)))
+            {
+                var text = (string)e.DataObject.GetData(typeof(string));
+                if (!IsTextAllowed(text))
+                {
+                    e.CancelCommand();
+                }
+            }
+            else
+            {
+                e.CancelCommand();
+            }
+        }
+
+        private void TextOtp_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            var id = 0;
+            var textBox = sender as TextBox;
+
+            if (sender is TextBox)
+            {
+                textBox.IsEnabled = false;
+                id = int.Parse(textBox.Name.Last().ToString());
+                textBox = (TextBox)FindName($"TextOtp{id + 1}");
+            }
+            if (id != 6)
+                textBox?.Focus();
+        }
+
+        #endregion
     }
 }
--- BusinessLogicWPF/View/LoginAndRegister/UserControls/LoginSelection.xaml.cs
+++ BusinessLogicWPF/View/LoginAndRegister/UserControls/LoginSelection.xaml.cs
@@ -1,18 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BusinessLogicWPF.ViewModel.LoginAndRegister;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using BusinessLogicWPF.ViewModel.LoginAndRegister;
 
 namespace BusinessLogicWPF.View.LoginAndRegister.UserControls
 {
@@ -31,12 +19,12 @@ namespace BusinessLogicWPF.View.LoginAndRegister.UserControls
 
         private void ButtonLoginAsAdmin_Click(object sender, RoutedEventArgs e)
         {
-
+            _window.DataContext = new LoginAsAdminViewModel();
         }
 
         private void ButtonLoginAsStationMaster_Click(object sender, RoutedEventArgs e)
         {
-
+            _window.DataContext = new LoginAsStationMasterViewModel();
         }
 
         private void ButtonLoginAsTte_Click(object sender, RoutedEventArgs e)
