21d9959a6d94f25e783cb6373f58182eb1ee35bf
Divisha1210 <divisha1210@gmail.com>
2019-03-09 19:38:16 +0530

    change in book ticket layout
    change done

---
+++ TicketChecker/app/src/main/java/com/example/newu/ticketchecker/BookTicket.java
@@ -0,0 +1,13 @@
+package com.example.newu.ticketchecker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BookTicket extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_ticket);
+    }
+}
