0f248fabfe880cb8a66aa3d036f256c4ab8a333a
Sourodeep Chatterjee <sourodeep224@outlook.com>
2019-04-13 20:11:26 +0530

    Added new Linq Extension Method DistinctBy

---
+++ BusinessLogicWPF/ExtensionMethods/GeneralPropertyComparer.cs
@@ -0,0 +1,86 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="GeneralPropertyComparer.cs" company="SDCWORLD">
+//   Sourodeep Chatterjee
+// </copyright>
+// <summary>
+//   Defines the GeneralPropertyComparer type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace BusinessLogicWPF.ExtensionMethods
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// The general property comparer.
+    /// </summary>
+    /// <typeparam name="T">
+    /// The T
+    /// </typeparam>
+    /// <typeparam name="TKey">
+    /// The TKey
+    /// </typeparam>
+    public class GeneralPropertyComparer<T, TKey> : IEqualityComparer<T>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GeneralPropertyComparer{T,TKey}"/> class.
+        /// </summary>
+        /// <param name="expr">
+        /// The expression.
+        /// </param>
+        public GeneralPropertyComparer(Func<T, TKey> expr)
+        {
+            this.Expr = expr;
+        }
+
+        /// <summary>
+        /// Gets the expression.
+        /// </summary>
+        private Func<T, TKey> Expr { get; }
+
+        /// <summary>
+        /// The equals.
+        /// </summary>
+        /// <param name="left">
+        /// The left.
+        /// </param>
+        /// <param name="right">
+        /// The right.
+        /// </param>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
+        public bool Equals(T left, T right)
+        {
+            var leftProp = this.Expr.Invoke(left);
+            var rightProp = this.Expr.Invoke(right);
+            if (leftProp == null && rightProp == null)
+            {
+                return true;
+            }
+
+            if (leftProp == null ^ rightProp == null)
+            {
+                return false;
+            }
+
+            return leftProp.Equals(rightProp);
+        }
+
+        /// <summary>
+        /// The get hash code.
+        /// </summary>
+        /// <param name="obj">
+        /// The object.
+        /// </param>
+        /// <returns>
+        /// The <see cref="int"/>.
+        /// </returns>
+        public int GetHashCode(T obj)
+        {
+            var prop = this.Expr.Invoke(obj);
+            return prop == null ? 0 : prop.GetHashCode();
+        }
+    }
+}
---
+++ BusinessLogicWPF/ExtensionMethods/LinqExtensions.cs
@@ -0,0 +1,45 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="LinqExtensions.cs" company="SDCWORLD">
+//   Sourodeep Chatterjee
+// </copyright>
+// <summary>
+//   Defines the LinqExtensions type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace BusinessLogicWPF.ExtensionMethods
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// The LINQ extensions.
+    /// </summary>
+    public static class LinqExtensions
+    {
+        /// <summary>
+        /// The distinct by.
+        /// </summary>
+        /// <param name="items">
+        /// The items.
+        /// </param>
+        /// <param name="property">
+        /// The property.
+        /// </param>
+        /// <typeparam name="T">
+        /// The T
+        /// </typeparam>
+        /// <typeparam name="TKey">
+        /// The YKey
+        /// </typeparam>
+        /// <returns>
+        /// The <see cref="IEnumerable{T}"/>.
+        /// </returns>
+        public static IEnumerable<T> DistinctBy<T, TKey>(this IEnumerable<T> items, Func<T, TKey> property)
+        {
+            var comparer = new GeneralPropertyComparer<T, TKey>(property);
+            return items.Distinct(comparer);
+        }   
+    }
+}
